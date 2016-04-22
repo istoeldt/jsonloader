5e87f21210ce7b30a45c99583b1f77b25f9ba781
Ingo Stoeldt <stoeldt@apt-solutions.de>
2016-04-22 15:09:43 +0200

    Initial Commit

---
+++ jsonloader/src/main/java/de/apt/jsonloader/jsonloader/App.java
@@ -0,0 +1,13 @@
+package de.apt.jsonloader.jsonloader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
